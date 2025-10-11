eee045b07809d225386be64dbfe6a098d60ba7a4
Jean BOURGUIGNON <jeanbourguignon.bzh@gmail.com>
2025-10-11 01:05:55 -0400

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# ASH - AutoShell
+
+## MVP:
+- [ ] Add parameter substitution
+- [ ] Add basic configs
+    - [ ] Customizable PS1
+- [ ] Autocomplete / Suggestions
+
+
+## Potential Future Plans:
+- [ ] Add color customization
+- [ ] Add "cursor" customization (The pre-prompt)
+- [ ] Add UI and extra info customaztion (Stuff around the cursor, like time)
---
+++ main.py
@@ -0,0 +1,64 @@
+# IMPORTS
+import os
+import yaml
+import subprocess
+import json
+
+# INIT
+commandLegend = {}
+ashConfig = {}
+with open("./ash.config.yaml", "r") as ashConfigFile:
+    rawCommandLegend = yaml.safe_load(ashConfigFile)
+    ashConfig = rawCommandLegend["ash-config"]
+    del rawCommandLegend["ash-config"]
+    commandLegend = rawCommandLegend
+
+# # PROMPT HANDLER
+# PROMPT_LEGEND = {
+#     "\u": "username",
+#     "\h": "hostname?",
+#     "\H": "full hostname",
+#     "\w": "Full working dir",
+#     "\W": "Basename working dr"
+# }
+# def parsePrompt(rawPrompt):
+#     for key in PROMPT_LEGEND.keys():
+#         rawPrompt.replace(key, )
+
+# LOGIC
+while True:
+    try:
+        userEnteredCommand = input(ashConfig["prompt"])
+        terminalCommand, *terminalArgs = userEnteredCommand.split(" ")
+
+        if terminalCommand == "ash-exit" or terminalCommand == "!!": break
+        
+        if terminalCommand.split(":")[0] in commandLegend.keys():
+            try:
+                if ":" in terminalCommand:
+                    terminalCommandRoot, *terminalCommandBranch = terminalCommand.split(":")
+                    # Compound Command
+
+                    keys = terminalCommand.split(":")
+                    node = commandLegend
+                    
+                    for idx, key in enumerate(keys):
+                        node = node.get(key)
+                        if node is None:
+                            break
+                        if idx < len(keys) - 1:
+                            node = node.get('subcmd', {})
+                    cmd = node.get('cmd') if isinstance(node, dict) else None
+
+                    print(f"Custom: {terminalCommand}; CMD: {cmd}")
+                else:
+                    # Basic Command
+                    print(f"Custom: {terminalCommand}; CMD: {commandLegend[terminalCommand]['cmd']}")
+            except:
+                print(f"Error - \"{terminalCommand}\" is not recognized as a custom command (No `cmd:` in YAML)")
+        else:
+            print(f"Native: {terminalCommand}; CMD: [Native]")
+
+    except KeyboardInterrupt:
+        print("\n[Cancelled]")
+        break
